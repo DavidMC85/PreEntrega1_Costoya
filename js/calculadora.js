5e985c2f3be44c2f83b8ba916260e437abbd1920
David Costoya <davidmartincostoya@gmail.com>
2023-03-11 21:45:49 -0300

    funciones_modificadas

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,103 +1,114 @@
-//--variables
+const OPERACIONES = {
+  SUMA: '+',
+  RESTA: '-',
+  MULTIPLICACION: '*',
+  DIVISION: '/'
+};
+
+const SALUDOS = {
+  TEMPRANPO: 'Buenos dias!',
+  TARDE: 'Buenas tardes!',
+  NOCHE: 'Buenas nochieee!'
+};
+
+const HORA_TEMPRANO_MAX = 12;
+const HORA_TARDE_MAX = 19;
+const HORA_NOCHE_MAX = 19;
+const HORA_MAXIMA = 24;
 
 var valorVisor = 0;
 var numeroA;
 var numeroB;
 var operacion;
-var ahora = new Date;
+var ahora = new Date();
 
-    //asignamiento de valores a las teclas
 function boton(tecla) {
-    var auxiliar = document.getElementById("visor").value;
-    document.getElementById("visor").value = auxiliar + tecla; 
+  var auxiliar = document.getElementById("visor").value;
+  document.getElementById("visor").value = auxiliar + tecla;
 
+  valorVisor = document.getElementById("visor").value = auxiliar + tecla;
 
-    valorVisor = document.getElementById("visor").value = auxiliar + tecla;
-    
-    // saludos
-    var hora = ahora.getHours();
-    
-    if(hora >= 0 && hora <= 12){
-      document.getElementById("historico").textContent = "Bien.. Arrancamos antes de las 12..";
+  // saludos
+  var hora = ahora.getHours();
+  var hora = ahora.getHours();
+
+  if (!localStorage.getItem("saludoMostrado")) {
+    if (hora >= 0 && hora <= 12) {
+      alert("Buenos dias!")
+      console.log("Madrugó este..")
+    } else if (hora >= 12 && hora <= 19) {
+      alert("Buenas tardes!")
+      console.log("Medio tarde arrancamos.. Como te llamas..? Ferran? xP");
+    } else {
+      alert("Buenas nochieee!")
+      console.log("Me duermo...")
     }
-    if(hora >= 13 && hora <= 16){
-      document.getElementById("historico").textContent = "Recien te levantas? Vago de m..";
-    }
-    if(hora >= 16 && hora <= 19){
-      document.getElementById("historico").textContent = "Que querés a esta hora?";
-    }
-    if(hora >= 20 && hora <= 23){
-      document.getElementById("historico").textContent = "Prepará un fernee viejaaa";
-    }
+
+    localStorage.setItem("saludoMostrado", "true");
+  }
 }
 
-
-function btn_suma(caracter){
-    numeroA = valorVisor;
-    operacion = "+";
-    
-    limpar();
-}
-
-function btn_resta(caracter){
+function btn_suma(caracter) {
   numeroA = valorVisor;
-  operacion = "-";
-  
+  operacion = SUMA;
+
   limpar();
 }
 
-function btn_multiplicacion(caracter){
+function btn_resta(caracter) {
   numeroA = valorVisor;
-  operacion = "*";
-  
+  operacion = RESTA;
+
   limpar();
 }
 
-function btn_division(caracter){
+function btn_multiplicacion(caracter) {
   numeroA = valorVisor;
-  operacion = "/";
-  
+  operacion = MULTIPLICACION;
+
   limpar();
 }
 
-    // limpar visor
+function btn_division(caracter) {
+  numeroA = valorVisor;
+  operacion = DIVISION;
+
+  limpar();
+}
+
 function reset() {
-    document.getElementById('visor').value = '';
-    valorVisor = 0;
-    operacion = "";
-}
-function limpar(){
-    document.getElementById('visor').value = '';
+  document.getElementById('visor').value = '';
+  valorVisor = 0;
+  operacion = '';
 }
 
-function btn_igual(){
-    numeroB = valorVisor;
-    resultado();
+function limpar() {
+  document.getElementById('visor').value = '';
+}
+
+function btn_igual() {
+  numeroB = valorVisor;
+  resultado();
 }
 
 function resultado() {
-
-    var total = 0;
-    var ultimoTotal = 0;
-      switch(operacion){
-        case "+":
-          total = parseFloat(numeroA) + parseFloat(numeroB);
-          break;
-        case "-":
-            total = parseFloat(numeroA) - parseFloat(numeroB);
-            break;
-        case "*":
-          total = parseFloat(numeroA) * parseFloat(numeroB);
-          break;
-        case "/":
-          total = parseFloat(numeroA) / parseFloat(numeroB);
-          break;
-      }
-      ultimoTotal = total;
-      reset();
-      document.getElementById('visor').value = total;
-      valorVisor = ultimoTotal;
-
+  var total = 0;
+  var ultimoTotal = 0;
+  
+  if (operacion === "+") {
+    total = parseFloat(numeroA) + parseFloat(numeroB);
+  } else if (operacion === "-") {
+    total = parseFloat(numeroA) - parseFloat(numeroB);
+  } else if (operacion === "*") {
+    total = parseFloat(numeroA) * parseFloat(numeroB);
+  } else if (operacion === "/") {
+    total = parseFloat(numeroA) / parseFloat(numeroB);
+  }
+  
+  ultimoTotal = total;
+  reset();
+  document.getElementById('visor').value = total;
+  valorVisor = ultimoTotal;
 }
 
 
